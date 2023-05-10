7421dba36164bccf7626c8c15dfca608bd8cc918
bharadvaj <chittalurus@hyd.adventglobal.com>
2023-05-10 16:38:20 +0530

    do some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
+
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(212, 45%, 89%)",
+      grayishBlue: "hsl(220, 15%, 55%)",
+      darkBlue: "hsl(218, 44%, 22%)",
+    },
+
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
